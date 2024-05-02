285e207a5c957e6470626ad4ecfcaa313a0cb5b1
Наталья Тайгачева <dupist@Air-Natala.bbrouter>
2024-05-02 17:31:37 +0700

    Change db.py, remove some enviroments variables

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -9,11 +9,6 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        dbname=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        host=os.getenv('DB_HOST')
-    )
+    conn = psycopg2.connect(DATABASE_URL)
     conn.cursor_factory = extras.DictCursor
     return conn
